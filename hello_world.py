88587d0c4c1e18df825914377b62ee2716676bda
mthmai <matheuspereiramai@gmail.com>
2022-12-26 09:11:29 -0300

    Transformação da tabuada em função

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,8 @@
-print('Hello World')
+def tabuada(n):
+	#n= int(input(print('Insira um numero:')))
 
+	for i in range (0,11):
+		print(f'{i}x{n}= {i*n}')
+	print(f'Essa e a tabuada do {n}')
 
-n= int(input(print('Insira um numero:')))
-
-for i in range (0,11):
-	print(f'{i}x{n}= {i*n}')
-print(f'Essa e a tabuada do {n}')
-
-print('Nova troca pra testar o pull')
 
